792154f887c4a1dd40eb84e44582b7699c861a9d
Łukasz Kuryło <mail>
2026-03-22 22:24:55 +0100

    fix updating db

---
+++ Booking.Infrastructure/Migrations/20260322212155_FixDbStructure.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Booking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixDbStructure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Barbers",
+                keyColumn: "Id",
+                keyValue: new Guid("4f624835-a45d-4de7-a986-7f319b77ea34"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Barbers",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { new Guid("4f624835-a45d-4de7-a986-7f319b77ea34"), "Jan Kowalski" });
+        }
+    }
+}
