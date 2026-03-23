9161789ad5f2b98872a05328160473747aac0a8e
Łukasz Kuryło <mail>
2026-03-23 18:42:04 +0100

    added comment to code

--- Booking.Application/Queries/ListBarbersQuery.cs
+++ Booking.Application/Queries/ListBarbersQuery.cs
@@ -8,8 +8,13 @@ public record ListBarbersQuery : IRequest<List<ListBarberDto>>;
 
 public class ListBarbersQueryHandler(IBarberRepository barberRepository) : IRequestHandler<ListBarbersQuery, List<ListBarberDto>>
 {
-    public async Task<List<ListBarberDto>> Handle(ListBarbersQuery request, CancellationToken cancellationToken)
+    public async Task<List<ListBarberDto>> Handle(
+        ListBarbersQuery request, CancellationToken cancellationToken)
     {
+        // in queries EF Core dbcontext can be used directly, because there is no business
+        // logic so there is no point in going through the repository and domain objects,
+        // just data retrieval, but for consistency (at least for now)
+        // I will use repository here as well
         var barbers = await barberRepository.GetAllAsync();
 
         return [.. barbers.Select(b => new ListBarberDto
